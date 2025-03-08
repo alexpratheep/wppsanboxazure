0a072be23fd720a06aeaa919cdb4087dbaa71428
Alexander Pratheep M <76117956+alexpratheep@users.noreply.github.com>
2025-03-08 12:59:42 +0530

    Update RG.tf

--- RG.tf
+++ RG.tf
@@ -1,8 +1,12 @@
 provider "azurerm" {
+subscription_id = var.SUBSCRIPTION_ID
+cliect_ID       = var.CLIENT_ID
+client_secret   = var.CLIENT_SECRET
+tennant_ID      = var.TENANT_ID
   features {}  # Required for the azurerm provider
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example"
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks"
   location = "West Europe"
 }
