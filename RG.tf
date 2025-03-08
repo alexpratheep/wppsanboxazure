afe4ad245675692dcfab0dd546cf674c53a79ba6
Alexander Pratheep M <76117956+alexpratheep@users.noreply.github.com>
2025-03-08 14:53:09 +0530

    Update RG.tf

--- RG.tf
+++ RG.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-subscription_id = var.subscription_id
-cliect_ID       = var.cliect_ID
-client_secret   = var.client_secret
-tennant_ID      = var.tennant_ID
+subscription_id = "b7591fe8-92c2-4214-94ea-157d955cc728"
+cliect_ID       = "ef8a1f82-cc41-4380-b770-dba6baeb5517"
+client_secret   = "3fd3ff6c-d636-4914-ba69-8956ed3b7311"
+tennant_ID      = "9729e73e-b2fc-4148-94e5-b6e7a0016dce"
   features {}  # Required for the azurerm provider
 }
 
